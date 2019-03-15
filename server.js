181a9986bf60d614f46727f8deccf8b155951608
Oliver Butanowitz <oliver.butanowitz@computacenter.com>
2019-03-15 13:55:58 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const os = require("os");
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+var HOSTNAME = os.hostname();
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send(`Hello world - running on ${HOSTNAME} \n`);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
